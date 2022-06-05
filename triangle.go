f521190fd9bb5bdf2a1524557b1c70be4fc1c8f6
William Smith <75560670+Tykhist@users.noreply.github.com>
2022-06-04 20:52:53 -0700

    Update triangle.go
    Added updateBase() function that uses a pointer to modify the Triangle struct.

--- triangle.go
+++ triangle.go
@@ -8,13 +8,20 @@ type Triangle struct {
 }
 
 func (triangle Triangle) area() float32{
-  return (triangle.base * triangle.height) / 2
+ 	return (triangle.base * triangle.height) / 2
+}
+
+func (triangle *Triangle) updateBase(newBase float32){
+  triangle.base = newBase
 }
 
 func main() {
-
 	triangle := Triangle{10, 4}
-  fmt.Println(triangle)
-
-  fmt.Println(triangle.area())
+	
+ 	fmt.Println(triangle)
+	fmt.Println(triangle.area())
+	
+	triangle.updateBase(13)
+	fmt.Println(triangle)
+	fmt.Println(triangle.area())
 }
