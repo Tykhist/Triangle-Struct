674cec267d698f76c5abb5d26ba85107150c4955
William Smith <75560670+Tykhist@users.noreply.github.com>
2022-06-04 20:05:51 -0700

    Create triangle.go

---
+++ triangle.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Triangle struct {
+	height float32
+	base float32
+}
+
+func (triangle Triangle) area() float32{
+  return (triangle.base * triangle.height) / 2
+}
+
+func main() {
+
+	triangle := Triangle{10, 4}
+  fmt.Println(triangle)
+
+  fmt.Println(triangle.area())
+}
