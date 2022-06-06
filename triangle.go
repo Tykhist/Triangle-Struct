fa0f97322cac2a24588063e58737d80df462743b
William Smith <mr.williamjdsmith@gmail.com>
2022-06-05 23:28:33 -0700

    Update triangle.go
    Added comments to explain the struct and functions

--- triangle.go
+++ triangle.go
@@ -3,15 +3,18 @@ package main
 import "fmt"
 
 type Triangle struct {
+	// Creates a data type with fields for a triangle
 	height float32
 	base float32
 }
 
-func (triangle Triangle) area() float32{
+func (triangle Triangle) area() float32 {
+	// Calculates the area of the triangle
  	return (triangle.base * triangle.height) / 2
 }
 
-func (triangle *Triangle) updateBase(newBase float32){
+func (triangle *Triangle) updateBase(newBase float32) {
+	// Updates the base field using a pointer for the Triangle struct
 	triangle.base = newBase
 }
 
